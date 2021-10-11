7b29d57445eb8814c6a798dfddace6bee5517d71
Jan Fredrik Bråstad <janfb@uia.no>
2021-10-11 21:16:34 +0200

    added a new branch

---
+++ objectDetection.py
@@ -0,0 +1,14 @@
+import cv2
+
+
+# objectDetection
+# Parameters:   needle_image, image we search for
+#               hay_image,    image we look in
+# Return value: Position as (x, y)
+
+def objectDetection(needle_image, hay_image):
+
+
+
+    pass
+
